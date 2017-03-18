2590b254bd38692af9b949bfc654a4e48d4d476c
Angular CLI <angular-cli@angular.io>
2017-03-18 15:52:00 +0100

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { UnisportFrontendPage } from './app.po';
+
+describe('unisport-frontend App', () => {
+  let page: UnisportFrontendPage;
+
+  beforeEach(() => {
+    page = new UnisportFrontendPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
