9c4ff623e280ce3567cac43b12d84c7989a1a747
Knut Hühne <knut@k-nut.eu>
2019-07-25 08:19:42 +0200

    Port most recent angular configuration
    - Copy configuration files over from an up to date angular project
    - Run unit and e2e test with Travis CI

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,5 @@
 // Karma configuration file, see link for more information
-// https://karma-runner.github.io/0.13/config/configuration-file.html
+// https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
   config.set({
@@ -12,42 +12,28 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    customLaunchers: {
-      Chrome_travis_ci: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
-    },
-    client:{
+    client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    files: [
-      
-    ],
-    preprocessors: {
-      
-    },
-    mime: {
-      'text/x-typescript': ['ts','tsx']
-    },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
+      dir: require('path').join(__dirname, './coverage/unisport-frontend'),
+      reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    
-    reporters: config.angularCli && config.angularCli.codeCoverage
-              ? ['progress', 'coverage-istanbul']
-              : ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu']
+      }
+    },
+    singleRun: false,
+    restartOnFileChange: true
   });
-
-  if (process.env.TRAVIS) {
-    config.browsers = ['Chrome_travis_ci'];
-    config.singleRun = true;
-  }
 };
+
--- src/app/resultsAge.service.spec.ts
+++ src/app/resultsAge.service.spec.ts
@@ -5,31 +5,28 @@ import {ResultsAgeService} from './resultsAge.service';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('MockBackend HeroService Example', () => {
+  let httpTestingController: HttpTestingController;
+  let resultsAgeService: ResultsAgeService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
-        HttpClientModule,
         HttpClientTestingModule
       ],
       providers: [ResultsAgeService]
     });
-    this.injector = getTestBed();
-    this.resultsAgeService = this.injector.get(ResultsAgeService);
-    this.httpMock = this.injector.get(HttpTestingController);
-  });
-
-  afterEach(() => {
-    this.httpMock.verify();
+    resultsAgeService = TestBed.get(ResultsAgeService);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
 
   it('getAge() should call /age url  parse date', fakeAsync(() => {
-    this.resultsAgeService.getAge().subscribe((age: Date) => {
+    resultsAgeService.getAge().subscribe((age: Date) => {
       const result = age;
       expect(result.getDate()).toEqual(7);
       expect(result.getFullYear()).toEqual(2016);
       expect(result.getMonth()).toEqual(3);
     });
-    const req = this.httpMock.expectOne('https://backend.unisport.berlin/age');
+    const req = httpTestingController.expectOne('https://backend.unisport.berlin/age');
     expect(req.request.method).toBe('GET');
     req.flush('2016-04-07 12:00:00.1234')
   }));
