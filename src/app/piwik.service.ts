ed526f8a4a62922e455cb65a7a05e157758b0c55
Knut Hühne <knut@k-nut.eu>
2020-09-21 11:17:49 +0200

    Move code to disable cookies to initialization

--- src/app/piwik.service.ts
+++ src/app/piwik.service.ts
@@ -9,7 +9,6 @@ export class PiwikService {
 
   trackSiteSearch(term, numberOfResults) {
     const window = getWindow();
-    window._paq.push(['disableCookies']);
     window._paq.push(['trackSiteSearch',
       term,
       false,
