7391501b2ad2db585ec17a61e0bbe6a500d5dde1
Knut Hühne <knut@k-nut.eu>
2022-03-14 16:08:30 +0100

    Update to angular 13

--- src/app/sportsClasses.service.ts
+++ src/app/sportsClasses.service.ts
@@ -1,13 +1,12 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpResponse, HttpParams, HttpErrorResponse} from '@angular/common/http';
 
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/map';
 
 import {SportsClass, Day, ISportsClassResponse, Location} from './models';
 
 import {HttpParameterCodec} from '@angular/common/http'
+import {Observable} from 'rxjs';
+import {catchError, map} from 'rxjs/operators';
 
 /**
  * A `HttpParameterCodec` that uses `encodeURIComponent` and `decodeURIComponent` to
@@ -16,13 +15,21 @@ import {HttpParameterCodec} from '@angular/common/http'
  * @stable
  */
 export class WebHttpUrlEncodingCodec implements HttpParameterCodec {
-  encodeKey(k: string): string { return encodeURIComponent(k); }
+  encodeKey(k: string): string {
+    return encodeURIComponent(k);
+  }
 
-  encodeValue(v: string): string { return encodeURIComponent(v); }
+  encodeValue(v: string): string {
+    return encodeURIComponent(v);
+  }
 
-  decodeKey(k: string): string { return decodeURIComponent(k); }
+  decodeKey(k: string): string {
+    return decodeURIComponent(k);
+  }
 
-  decodeValue(v: string) { return decodeURIComponent(v); }
+  decodeValue(v: string) {
+    return decodeURIComponent(v);
+  }
 }
 
 export enum BookingStatus {
@@ -44,12 +51,13 @@ export class SportsClassService {
   private baseUrl = 'https://api.unisport.berlin'
   private sportsClassUrl = `${this.baseUrl}/classes`;
   private namesUrl = `${this.baseUrl}/names`;
-  private locationsUrl =  `${this.baseUrl}/locations`;
+  private locationsUrl = `${this.baseUrl}/locations`;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   getSportsClasses(options: ISearchOptions = {bookable: BookingStatus.all}): Observable<SportsClass[]> {
-    let params: HttpParams = new HttpParams({encoder: new WebHttpUrlEncodingCodec() });
+    let params: HttpParams = new HttpParams({encoder: new WebHttpUrlEncodingCodec()});
     if (options.name) {
       params = params.set('name', options.name);
     }
@@ -67,21 +75,25 @@ export class SportsClassService {
       params = params.set('location_url', options.locationUrl);
     }
     return this.http.get<ISportsClassResponse>(this.sportsClassUrl, {params})
-      .map(this.extractData)
-      .catch(this.handleError);
+      .pipe(
+        map(this.extractData),
+        catchError(this.handleError)
+      );
   }
 
   getNames() {
-    return this.http.get<{data: string[]}>(this.namesUrl)
-      .map(json => json.data)
-      .catch(this.handleError);
+    return this.http.get<{ data: string[] }>(this.namesUrl)
+      .pipe(
+        map(json => json.data),
+        catchError(this.handleError)
+      );
   }
 
   private extractData(json: ISportsClassResponse) {
     return json.data.map(sc => new SportsClass(sc));
   }
 
-  private handleError (error: HttpErrorResponse | any) {
+  private handleError(error: HttpErrorResponse | any) {
     // In a real world app, you might use a remote logging infrastructure
     let errMsg: string;
     if (error instanceof HttpErrorResponse) {
@@ -95,8 +107,10 @@ export class SportsClassService {
   }
 
   getLocations() {
-    return this.http.get<{data: Location[]}>(this.locationsUrl)
-      .map(json => json.data)
-      .catch(this.handleError);
+    return this.http.get<{ data: Location[] }>(this.locationsUrl)
+      .pipe(
+        map(json => json.data),
+        catchError(this.handleError)
+      );
   }
 }
